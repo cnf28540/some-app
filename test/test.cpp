59d95fb8419380dfff12ef3e9532a0fd2e52f59c
Some guy <cnf28540@gmail.com>
2020-05-15 12:26:25 -0400

    Added in Google Test

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include "gtest/gtest.h"
+
+namespace {
+
+TEST(SomeTest, TestA) {
+	EXPECT_EQ(1,1);
+}
+
+}
 
 int main(int argc, char *argv[]){
+	int result = 0;
+
 	std::cout << "Running test!" << std::endl;
+
+	testing::InitGoogleTest();
+
+	result = RUN_ALL_TESTS();
+
 	return 0;
 }
